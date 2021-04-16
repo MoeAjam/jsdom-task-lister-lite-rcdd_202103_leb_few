03a5832d50d596b5c03879cba5fe878d1ea77bb1
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-16 17:46:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let btn = document.getElementsByTagName('input')[1];
   let taskList = document.getElementById('tasks');
   btn.addEventListener('click', fucntion(e){
-    taskList.innerHTML += "<li>sdfs</li>";
+    taskList.innerHTML += `<li>${txt.textContent}</li>`;
     event.preventDefault();
   }
 }
