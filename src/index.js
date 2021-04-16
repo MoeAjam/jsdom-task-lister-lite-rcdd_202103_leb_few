ac9de48d8d6453fad3b24cd36be13819fd5c692f
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-16 17:37:41 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  txt = document.getElementById('new-task-description');
+  btn = document.getElementsByTagName('input')[1];
+  taskList = document.getElementById('tasks');
+  btn.addEventListener('click',()=>e {
+    
+  })
 });
